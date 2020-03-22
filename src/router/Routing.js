03715ef9e16c48c7ed1e4f0d96bf9e34986b247d
Joshua Chew <chewzhj@gmail.com>
2020-03-22 23:37:45 +0800

    New Pages and color change of sidebar

--- src/router/Routing.js
+++ src/router/Routing.js
@@ -3,13 +3,24 @@ import {Route, Switch} from 'react-router-dom'
 import DefaultReactApp from '../assets/js/App'
 import CounterContainer from '../containers/CounterContainer'
 import LoginContainer from '../containers/LoginContainer'
+import Dashboard from '../views/Dashboard'
+import Users from '../views/Users'
+import QuizMain from '../views/QuizMain'
+import TagsMain from '../views/TagsMain'
+import InsightsMain from '../views/InsightsMain'
 import SideBar from '../components/SideBar'
 
 const Routing = () => {
   return (
     <Switch>
 
-      <Route path="/layout" component={SideBar}/>
+      {/* <Route path="/index" component={SideBar}/> */}
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/users" component={Users} />
+      <Route path="/quizzes" component={QuizMain} />
+      <Route path="/tags" component={TagsMain} />
+      <Route path="/insights" component={InsightsMain} />
+
 
       <Route path="/counter" component={CounterContainer}/>
       <Route path="/react-default" component={DefaultReactApp}/>
---
+++ src/views/QuizMain.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../components/SideBar'
+
+export default class QuizMain extends React.Component {
+
+
+  render() {
+    return (
+      <SideBar activeTab='quiz'>
+        Quizzes
+      </SideBar>
+    )
+  }
+}
