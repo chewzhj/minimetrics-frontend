605e9684175a70eaf6a9bdb784ab810eb64bcdf0
Lawrence Lim <e0176571@u.nus.edu>
2020-03-26 21:17:33 +0800

    added empty pages TODOL fix submenu Insights not being automatically opened

--- src/router/Routing.js
+++ src/router/Routing.js
@@ -9,6 +9,10 @@ import QuizMainContainer from '../containers/QuizMainContainer'
 import QuizCreationContainer from '../containers/QuizCreationContainer'
 import TagsMain from '../views/TagsMain'
 import InsightsMain from '../views/InsightsMain'
+import InsightsTopic from '../views/InsightsTopic'
+import InsightsQuiz from '../views/InsightsQuiz'
+import InsightsStudent from '../views/InsightsStudent'
+import InsightsConfidence from '../views/InsightsConfidence'
 import SideBar from '../components/SideBar'
 
 const Routing = () => {
@@ -22,7 +26,11 @@ const Routing = () => {
       <Route path="/quiz/create" component={QuizCreationContainer} />
       <Route path="/quiz" component={QuizMainContainer} />
       <Route path="/tags" component={TagsMain} />
-      <Route path="/insights" component={InsightsMain} />
+      <Route path="/insights/overview" component={InsightsMain} />
+      <Route path="/insights/topic" component={InsightsTopic} />
+      <Route path="/insights/quiz" component={InsightsQuiz} />
+      <Route path="/insights/student" component={InsightsStudent} />
+      <Route path="/insights/confidence" component={InsightsConfidence} />
 
 
       <Route path="/counter" component={CounterContainer}/>
---
+++ src/views/InsightsConfidence.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../components/SideBar'
+
+export default class InsightsConfidence extends React.Component {
+
+
+  render() {
+    return (
+      <SideBar activeTab='insights/confidence'>
+        Confidence Insights
+      </SideBar>
+    )
+  }
+}
