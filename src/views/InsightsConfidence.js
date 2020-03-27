a8f67e4ed80239db7cf75d0eb8ac4b3082cc964a
Lawrence Lim <e0176571@u.nus.edu>
2020-03-28 00:04:47 +0800

    populated confidence table on Confidence Insights page

--- src/views/InsightsConfidence.js
+++ src/views/InsightsConfidence.js
@@ -12,17 +12,136 @@ import {
 } from 'antd';
 import CommonPhrases from '../phrases/CommonPhrases'
 import InsightsPhrases from '../phrases/InsightsPhrases'
-import { QuestionCircleFilled, SmileTwoTone, CloseCircleTwoTone, CheckCircleTwoTone } from '@ant-design/icons'
+import { QuestionCircleFilled, SmileTwoTone, CloseCircleTwoTone, CheckCircleTwoTone, EyeOutlined } from '@ant-design/icons'
 import { ResponsivePie } from '@nivo/pie'
 
 const { Option } = Select;
 const { Title, Text } = Typography;
 
 const chartColors = ['#f79992', '#f78e3d', '#ffdd76', '#76d0a3'];
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Freq. in Misinformed',
+    dataIndex: 'freqMisinformed',
+  },
+  {
+    title: 'Freq. in Uninformed',
+    dataIndex: 'freqUninformed',
+  },
+  {
+    title: 'Freq. in Almost There',
+    dataIndex: 'freqAlmostThere',
+  },
+  {
+    title: 'Freq. in Knowledgeable',
+    dataIndex: 'freqKnowledgeable',
+  },
+  {
+    title: 'View',
+    render: (text, record, index) => {
+      return (
+        <Button shape='circle' icon={<EyeOutlined />} />
+      )
+    }
+  }
+];
 
 export default class InsightsConfidence extends React.Component {
 
+  generateTableData = () => {
+    const tableData = [
+      {
+        name: 'Robert Fischer',
+        userId: 1,
+        freqMisinformed: 33,
+        freqUninformed: 33,
+        freqAlmostThere: 32,
+        freqKnowledgeable: 33
+      },
+      {
+        name: 'Mitchell Robertson',
+        userId: 2,
+        freqMisinformed: 21,
+        freqUninformed: 21,
+        freqAlmostThere: 20,
+        freqKnowledgeable: 11
+      },
+      {
+        name: 'Ricardo Black',
+        userId: 3,
+        freqMisinformed: 20,
+        freqUninformed: 20,
+        freqAlmostThere: 20,
+        freqKnowledgeable: 11
+      },
+      {
+        name: 'Harold Edwards',
+        userId: 4,
+        freqMisinformed: 19,
+        freqUninformed: 12,
+        freqAlmostThere: 10,
+        freqKnowledgeable: 13
+      },
+      {
+        name: 'Jennie Warren',
+        userId: 5,
+        freqMisinformed: 16,
+        freqUninformed: 11,
+        freqAlmostThere: 13,
+        freqKnowledgeable: 12
+      },
+      {
+        name: 'Audrey Watson',
+        userId: 6,
+        freqMisinformed: 15,
+        freqUninformed: 11,
+        freqAlmostThere: 1,
+        freqKnowledgeable: 3
+      },
+      {
+        name: 'Judith Richards',
+        userId: 7,
+        freqMisinformed: 12,
+        freqUninformed: 3,
+        freqAlmostThere: 3,
+        freqKnowledgeable: 11
+      },
+      {
+        name: 'Soham Ngyuen',
+        userId: 8,
+        freqMisinformed: 11,
+        freqUninformed: 4,
+        freqAlmostThere: 2,
+        freqKnowledgeable: 5
+      },
+      {
+        name: 'Jacob Fox',
+        userId: 9,
+        freqMisinformed: 11,
+        freqUninformed: 4,
+        freqAlmostThere: 2,
+        freqKnowledgeable: 3
+      },
+      {
+        name: 'Freida Ignan',
+        userId: 10,
+        freqMisinformed: 7,
+        freqUninformed: 3,
+        freqAlmostThere: 3,
+        freqKnowledgeable: 2
+      }
+    ]
+
+    return tableData
+  }
+
   render() {
+    const tableData = this.generateTableData()
+
     return (
       <SideBar activeTab='insights/confidence' title="Insights" subtitle="Confidence Insights">
 
@@ -113,80 +232,86 @@ export default class InsightsConfidence extends React.Component {
 
         <Row gutter={[15, 15]} style={{ marginTop: 20, paddingLeft: 20, paddingRight: 20 }}>
 
-          <Col md={12} sm={24} xs={24}>
+          <Col md={6} sm={24} xs={24}>
             <Card style={{ width: '100%', borderRadius: 4, boxShadow: '2px 1px 4px 0px #bcbcbc', background: '#fcdbd9' }}>
               <Row justify="space-between">
-                <Text strong style={{ fontSize: 16 }}>Misinformed</Text>
+                <Text strong style={{ fontSize: 14 }}>Misinformed</Text>
                 <Tooltip placement="topLeft" title={InsightsPhrases.CONFIDENCE_INSIGHTS_EXPLN_GRP_MIS} arrowPointAtCenter>
-                  <QuestionCircleFilled style={{ fontSize: '24px' }} />
+                  <QuestionCircleFilled style={{ fontSize: 14, marginTop: 3 }} />
                 </Tooltip>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <SmileTwoTone twoToneColor="#f04134" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Confident</Text>
+                <SmileTwoTone twoToneColor="#f04134" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Confident</Text>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <CloseCircleTwoTone twoToneColor="#f04134" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Incorrect</Text>
+                <CloseCircleTwoTone twoToneColor="#f04134" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Incorrect</Text>
               </Row>
             </Card>
           </Col>
 
-          <Col md={12} sm={24} xs={24}>
+          <Col md={6} sm={24} xs={24}>
             <Card style={{ width: '100%', borderRadius: 4, boxShadow: '2px 1px 4px 0px #bcbcbc', background: '#fccca7' }}>
               <Row justify="space-between">
-                <Text strong style={{ fontSize: 16 }}>Uninformed</Text>
+                <Text strong style={{ fontSize: 14 }}>Uninformed</Text>
                 <Tooltip placement="topLeft" title={InsightsPhrases.CONFIDENCE_INSIGHTS_EXPLN_GRP_UNI} arrowPointAtCenter>
-                  <QuestionCircleFilled style={{ fontSize: '24px' }} />
+                  <QuestionCircleFilled style={{ fontSize: 14, marginTop: 3 }} />
                 </Tooltip>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <SmileTwoTone rotate="180" twoToneColor="#faaf76" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Not Confident</Text>
+                <SmileTwoTone rotate="180" twoToneColor="#faaf76" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Not Confident</Text>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <CloseCircleTwoTone twoToneColor="#f04134" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Incorrect</Text>
+                <CloseCircleTwoTone twoToneColor="#f04134" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Incorrect</Text>
               </Row>
             </Card>
           </Col>
 
-          <Col md={12} sm={24} xs={24}>
+          <Col md={6} sm={24} xs={24}>
             <Card style={{ width: '100%', borderRadius: 4, boxShadow: '2px 1px 4px 0px #bcbcbc', background: '#fff3cf' }}>
               <Row justify="space-between">
-                <Text strong style={{ fontSize: 16 }}>"Almost There"</Text>
+                <Text strong style={{ fontSize: 14 }}>"Almost There"</Text>
                 <Tooltip placement="topLeft" title={InsightsPhrases.CONFIDENCE_INSIGHTS_EXPLN_GRP_ALM} arrowPointAtCenter>
-                  <QuestionCircleFilled style={{ fontSize: '24px' }} />
+                  <QuestionCircleFilled style={{ fontSize: 14, marginTop: 3 }} />
                 </Tooltip>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <SmileTwoTone rotate="180" twoToneColor="#faaf76" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Not Confident</Text>
+                <SmileTwoTone rotate="180" twoToneColor="#faaf76" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Not Confident</Text>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <CheckCircleTwoTone twoToneColor="#52c41a" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Correct</Text>
+                <CheckCircleTwoTone twoToneColor="#52c41a" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Correct</Text>
               </Row>
             </Card>
           </Col>
 
-          <Col md={12} sm={24} xs={24}>
+          <Col md={6} sm={24} xs={24}>
             <Card style={{ width: '100%', borderRadius: 4, boxShadow: '2px 1px 4px 0px #bcbcbc', background: '#cfefdf' }}>
               <Row justify="space-between">
-                <Text strong style={{ fontSize: 16 }}>Knowledgeable</Text>
+                <Text strong style={{ fontSize: 14 }}>Knowledgeable</Text>
                 <Tooltip placement="topLeft" title={InsightsPhrases.CONFIDENCE_INSIGHTS_EXPLN_GRP_KNO} arrowPointAtCenter>
-                  <QuestionCircleFilled style={{ fontSize: '24px' }} />
+                  <QuestionCircleFilled style={{ fontSize: 14, marginTop: 3 }} />
                 </Tooltip>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <SmileTwoTone twoToneColor="#52c41a" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Confident</Text>
+                <SmileTwoTone twoToneColor="#52c41a" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Confident</Text>
               </Row>
               <Row style={{ marginTop: 10 }}>
-                <CheckCircleTwoTone twoToneColor="#52c41a" style={{ fontSize: '20px', marginTop: 1 }} />
-                <Text>&nbsp;&nbsp;Correct</Text>
+                <CheckCircleTwoTone twoToneColor="#52c41a" style={{ fontSize: '20px' }} />
+                <Text style={{ fontSize: 12, marginTop: 1 }}>&nbsp;&nbsp;Correct</Text>
               </Row>
             </Card>
+          </Col>
+        </Row>
+
+        <Row style={{ marginTop: 20, marginLeft: 20, marginRight: 20 }}>
+          <Col md={24} xs={24}>
+            <Table columns={columns} dataSource={tableData} bordered={true} />
           </Col>
         </Row>
       </SideBar>
